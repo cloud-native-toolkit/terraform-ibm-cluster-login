e59704b82908f3c0f7fdc830758dddfca21937c8
Sean Sundberg <seansund@us.ibm.com>
2023-03-30 17:28:47 -0500

    Update wait-for-iam-sync logic to work on IKS
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- scripts/wait-for-iam-sync.sh
+++ scripts/wait-for-iam-sync.sh
@@ -12,20 +12,6 @@ USERNAME=$(kubectl config current-context | sed -E "s~.*/(.+)~\1~g")
 
 echo "Checking for user: $USERNAME"
 
-count=0
-until [[ -n $(kubectl get user -o json | jq -r --arg NAME "${USERNAME}" '.items[].metadata.name | select(. == $NAME)') ]] || [[ $count -eq 20 ]]; do
-  count=$((count + 1))
-  echo "  Waiting for 30 seconds"
-  sleep 30
-done
-
-if [[ $count -eq 20 ]]; then
-  echo "Timed out waiting for user: $USERNAME" >&2
-  exit 1
-else
-  echo "  Found user: $USERNAME"
-fi
-
 echo "Waiting for role bindings for user: $USERNAME"
 
 function role_binding_count {
@@ -37,13 +23,13 @@ function role_binding_count {
 }
 
 count=0
-until [[ $(role_binding_count "${USERNAME}") -gt 0 ]] || [[ $count -eq 20 ]]; do
+until [[ $(role_binding_count "${USERNAME}") -gt 0 ]] || [[ $count -eq 40 ]]; do
   count=$((count + 1))
   echo "  Waiting for 30 seconds"
   sleep 30
 done
 
-if [[ $count -eq 20 ]]; then
+if [[ $count -eq 40 ]]; then
   echo "Timed out waiting for role bindings: $USERNAME" >&2
   exit 1
 else
